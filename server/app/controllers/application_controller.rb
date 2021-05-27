c9bd6519886f9ccd10c899c8818dc96a8dbc649f
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-05-28 07:45:10 +0900

    smartcampの内容を移したけどzeitwerkで動かない

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def ping
+    pinger_stub = Pinger::Pinger::Stub.new('localhost:5300', :this_channel_is_insecure)
+    pong = pinger_stub.ping(Pinger::Empty.new)
+    render json: {pong: pong.text}
+  end
 end
