750167ccd5fabefe40f17e53d676c95138e38462
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-05-28 08:20:31 +0900

    autoloader = :classicにして、無理やり動かした

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  require 'pinger_services_pb'
   def ping
     pinger_stub = Pinger::Pinger::Stub.new('localhost:5300', :this_channel_is_insecure)
     pong = pinger_stub.ping(Pinger::Empty.new)
--- server/config/application.rb
+++ server/config/application.rb
@@ -24,6 +24,14 @@ module Server
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # 絶対にやってはいけない設定。Zeitwerkをオフにする
+    config.autoloader = :classic
+
+    # ActiveSupport::Dependencies.autoload_paths
+    # lib配下を読ませる。どっちでもok
+    config.paths.add 'lib', eager_load: true
+    config.autoload_paths << Rails.root.join("lib")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
