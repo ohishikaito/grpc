7d95b6301eac0e0b34ee8cb2a7ae32ebd93d0198
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-05-29 22:05:09 +0900

    jsonでresponse返せました

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,20 +1,13 @@
 class UsersController < ApplicationController
-  # userを配列でとれるようにしたい
   def index
-    users = Protos::Stubs::UserStub.get_users(UserProto::Empty.new)
-    # render json: { users: users }
-    p users.users[0]
-    # binding.pry
-    # render json: { users: users.users }
-    render json: { users: users.users[0].to_json }
-    # render json: { users: users.name }
+    req = UserProto::Empty.new
+    users = Protos::Stubs::UserStub.get_users(req)
+    render json: users, status: :ok
   end
 
   def show
     req = UserProto::GetUserReq.new({id: 11})
-    p req
     user = Protos::Stubs::UserStub.get_user(req)
-    p user
-    render json: { user: user.email }
+    render json: user, status: :ok
   end
 end
